71713cea014f78207daa7c0ab20354853966eb43
andy <21304928@qq.com>
2017-12-09 13:23:17 +0800

    fix

--- common/base.sh
+++ common/base.sh
@@ -5,19 +5,19 @@ export BREW_HOME=~/workspace/tool/homebrew
 
 export HOMEBREW_CACHE=$BREW_HOME/cache
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
-export PATH=$PATH:$BREW_HOME/bin
+export PATH=$BREW_HOME/bin:$PATH
 
 export GOROOT=$TOOL_HOME/go/1.9.2
 export GOPATH=$HOME/workspace/code/go
-export PATH=$PATH:$GOROOT/bin
+export PATH=$GOROOT/bin:$PATH
 
-export PATH=$PATH:$TOOL_HOME/android/sdk/tools
-export PATH=$PATH:$TOOL_HOME/android/sdk/tools/bin
-export PATH=$PATH:$TOOL_HOME/android/sdk/platform-tools
+export PATH=$TOOL_HOME/android/sdk/tools:$PATH
+export PATH=$TOOL_HOME/android/sdk/tools/bin:$PATH
+export PATH=$TOOL_HOME/android/sdk/platform-tools:$PATH
 
-export PATH=$PATH:$TOOL_HOME/node/v8.9.0/bin
-export PATH=$PATH:~/Library/Python/2.7/bin
+export PATH=$TOOL_HOME/node/v8.9.0/bin:$PATH
+export PATH=~/Library/Python/2.7/bin:$PATH
 
 for item in `find $TOOL_HOME -name bin -type d -maxdepth 3`; do
-    export PATH=$PATH:$item
+    export PATH=$item:$PATH
 done
