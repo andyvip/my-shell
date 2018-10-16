3c895ba62572ad3a4df9d812af47c0f60b60e7aa
andy <andy@andys-MacBook-Pro.local>
2018-10-16 19:12:36 +0800

    xxx

--- myshell.sh
+++ myshell.sh
@@ -1,26 +1,18 @@
-if [[ $SHELL == "/bin/zsh" ]]; then
-	MYSHELL="$(cd "$(dirname "$0")" && pwd)"
-	ZSH=$MYSHELL/thirdparty/oh-my-zsh
-	ZSH_CUSTOM=$MYSHELL/custom/oh-my-zsh/custom
-	DISABLE_AUTO_UPDATE=false
-	DISABLE_UPDATE_PROMPT=false
-	plugins=(git)
-	source $ZSH/oh-my-zsh.sh
-fi
+export PS1="\$ \w > "
+export CLICOLOR=1
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export HOMEBREW_NO_AUTO_UPDATE=true
 
-if [[ $SHELL == "/bin/bash" ]]; then
-  export PS1="\$ \w > "
-  export CLICOLOR=1
-fi
+export _WORKSPACE=~/workspace
+export JAVA_HOME=$_WORKSPACE/tool/jdk-11.jdk/Contents/Home
+export ANDROID_SDK=$_WORKSPACE/android-sdk
+export ANDROID_NDK=$_WORKSPACE/tool/android-ndk-r14b
+
+export PATH=$_WORKSPACE/tool/homebrew/bin:$JAVA_HOME/bin:$PATH
+export PATH=$PATH:$ANDROID_SDK/platform-tools
 
 alias ll="ls -alF"
 alias subl="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-export HOMEBREW_NO_AUTO_UPDATE=true
-
-export PATH=$HOME/workspace/tool/android-sdk/platform-tools:$PATH
-
-export ANDROID_NDK=$HOME/workspace/tool/android-ndk-r14b
+[ -f ~/workspace/tool/homebrew/etc/bash_completion ] && . ~/workspace/tool/homebrew/etc/bash_completion
